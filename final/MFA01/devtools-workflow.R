962d3efcc6fc633c5d78e6c859f94c93828cbfab
Gaston Sanchez <gaston.stat@gmail.com>
2016-12-05 21:14:48 -0800

    separating devtools-workflow.R from install.R

--- final/MFA01/devtools-workflow.R
+++ final/MFA01/devtools-workflow.R
@@ -1,28 +1,13 @@
 #install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
 
 
-# setwd('/Users/Shiying/Dropbox/BERKELEY_study/2016_Fall/02_STAT243/243_proj/Stats-243/final')
-
-setwd('C:/Users/Emma/Downloads/stats-243/final/')
-
 library(devtools)
 
-pkg <- 'MFA01'
-
-devtools::document(pkg = pkg)
-devtools::check_man(pkg = pkg)
-devtools::test(pkg = pkg)
-devtools::build_vignettes(pkg = pkg)
-devtools::build(pkg = pkg)
-devtools::install(pkg = pkg)
+devtools::document()
+devtools::check_man()
+devtools::test()
+devtools::build_vignettes()
+devtools::build()
+devtools::install()
 
 library(MFA01)
-
-
-# --------------------below is function testing
-# a <- MFA()
-# a
-#
-# x <- matrix(1:100, nrow=10, ncol=10)
-# s1 <- list(1:3,5:6,7:10)
-# MFA(x,s1)
---
+++ final/MFA01/install.R
@@ -0,0 +1,28 @@
+#install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
+
+
+# setwd('/Users/Shiying/Dropbox/BERKELEY_study/2016_Fall/02_STAT243/243_proj/Stats-243/final')
+
+setwd('C:/Users/Emma/Downloads/stats-243/final/')
+
+library(devtools)
+
+pkg <- 'MFA01'
+
+devtools::document(pkg = pkg)
+devtools::check_man(pkg = pkg)
+devtools::test(pkg = pkg)
+devtools::build_vignettes(pkg = pkg)
+devtools::build(pkg = pkg)
+devtools::install(pkg = pkg)
+
+library(MFA01)
+
+
+# --------------------below is function testing
+# a <- MFA()
+# a
+#
+# x <- matrix(1:100, nrow=10, ncol=10)
+# s1 <- list(1:3,5:6,7:10)
+# MFA(x,s1)
